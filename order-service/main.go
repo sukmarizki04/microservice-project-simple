ae182c2d9ab0c6408dd65060e1a095801b34df02
sukmarizki04 <sukmarizki04@gmail.com>
2025-06-11 13:47:10 +0700

    seconds

--- order-service/main.go
+++ order-service/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var orders = map[string]interface{}{
+	"order001": map[string]interface{}{
+		"item":     "Laptop",
+		"quantity": 1,
+		"price":    1200.50,
+	},
+	"order002": map[string]interface{}{
+		"item":     "Mouse",
+		"quantity": 2,
+		"price":    25.99,
+	},
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/order", func(c *gin.Context) {
+		c.JSON(200, gin.H{"messge": "oder service", "orders": orders})
+	})
+	r.Run(":8081")
+}
