a2502633b1a39e6d22c632078f4dbc54d29b2cf1
sukmarizki04 <sukmarizki04@gmail.com>
2025-08-25 22:22:16 +0700

    25

--- simple_messaqque/main.go
+++ simple_messaqque/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 // using chanel
@@ -13,23 +15,26 @@ func worker(id int, jobs <-chan int, result chan<- int) {
 		result <- job * 2
 	}
 }
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbum(c *gin.Context) {
+}
 func main() {
-	jobs := make(chan int, 5)
-	result := make(chan int, 5)
-
-	//start working
-	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, result)
-	}
-
-	//Send the jobs
-	for j := 1; j <= 5; j++ {
-		jobs <- j
-	}
-	close(jobs)
-	//Collecftion  result
-	for a := 1; a <= 5; a++ {
-		fmt.Printf("Result: %d\n", <-result)
-	}
+	router := gin.Default()
+	router.GET("/albums", getAlbum)
+	router.Run("localhost:8080")
 
 }
